65a0096e041dc0fcbf147a17b9bfbce85c823392
dohyoung rim <dh-rim@hanmail.net>
2022-01-05 06:27:08 +0000

    yolov3_wrapper.py 추가

---
+++ yolov3_wrapper.py
@@ -0,0 +1,167 @@
+import cv2
+import numpy as np
+import core.utils as utils
+import core.common as common
+import tensorflow as tf
+from PIL import Image
+import matplotlib.pyplot as plt
+import colorsys
+import random
+
+def darknet53(input_data):
+
+    input_data = common.convolutional(input_data, (3, 3,  3,  32))
+    input_data = common.convolutional(input_data, (3, 3, 32,  64), downsample=True)
+
+    for i in range(1):
+        input_data = common.residual_block(input_data,  64,  32, 64)
+
+    input_data = common.convolutional(input_data, (3, 3,  64, 128), downsample=True)
+
+    for i in range(2):
+        input_data = common.residual_block(input_data, 128,  64, 128)
+
+    input_data = common.convolutional(input_data, (3, 3, 128, 256), downsample=True)
+
+    for i in range(8):
+        input_data = common.residual_block(input_data, 256, 128, 256)
+
+    route_1 = input_data
+    input_data = common.convolutional(input_data, (3, 3, 256, 512), downsample=True)
+
+    for i in range(8):
+        input_data = common.residual_block(input_data, 512, 256, 512)
+
+    route_2 = input_data
+    input_data = common.convolutional(input_data, (3, 3, 512, 1024), downsample=True)
+
+    for i in range(4):
+        input_data = common.residual_block(input_data, 1024, 512, 1024)
+
+    return route_1, route_2, input_data
+
+def YOLOv3(input_layer):
+    
+    NUM_CLASS = 80
+    
+    route_1, route_2, conv = darknet53(input_layer)
+
+    conv = common.convolutional(conv, (1, 1, 1024,  512))
+    conv = common.convolutional(conv, (3, 3,  512, 1024))
+    conv = common.convolutional(conv, (1, 1, 1024,  512))
+    conv = common.convolutional(conv, (3, 3,  512, 1024))
+    conv = common.convolutional(conv, (1, 1, 1024,  512))
+
+    conv_lobj_branch = common.convolutional(conv, (3, 3, 512, 1024))
+    conv_lbbox = common.convolutional(conv_lobj_branch, (1, 1, 1024, 3*(NUM_CLASS + 5)), activate=False, bn=False)
+
+    conv = common.convolutional(conv, (1, 1,  512,  256))
+    conv = common.upsample(conv)
+
+    conv = tf.concat([conv, route_2], axis=-1)
+
+    conv = common.convolutional(conv, (1, 1, 768, 256))
+    conv = common.convolutional(conv, (3, 3, 256, 512))
+    conv = common.convolutional(conv, (1, 1, 512, 256))
+    conv = common.convolutional(conv, (3, 3, 256, 512))
+    conv = common.convolutional(conv, (1, 1, 512, 256))
+
+    conv_mobj_branch = common.convolutional(conv, (3, 3, 256, 512))
+    conv_mbbox = common.convolutional(conv_mobj_branch, (1, 1, 512, 3*(NUM_CLASS + 5)), activate=False, bn=False)
+
+    conv = common.convolutional(conv, (1, 1, 256, 128))
+    conv = common.upsample(conv)
+
+    conv = tf.concat([conv, route_1], axis=-1)
+
+    conv = common.convolutional(conv, (1, 1, 384, 128))
+    conv = common.convolutional(conv, (3, 3, 128, 256))
+    conv = common.convolutional(conv, (1, 1, 256, 128))
+    conv = common.convolutional(conv, (3, 3, 128, 256))
+    conv = common.convolutional(conv, (1, 1, 256, 128))
+
+    conv_sobj_branch = common.convolutional(conv, (3, 3, 128, 256))
+    conv_sbbox = common.convolutional(conv_sobj_branch, (1, 1, 256, 3*(NUM_CLASS +5)), activate=False, bn=False)
+
+    return [conv_sbbox, conv_mbbox, conv_lbbox]
+
+def decode(conv_output, i=0):
+    """
+    return tensor of shape [batch_size, output_size, output_size, anchor_per_scale, 5 + num_classes]
+            contains (x, y, w, h, score, probability)
+    """
+
+    NUM_CLASS = 80
+    STRIDES = np.array([8, 16, 32])
+    
+    # anchors = np.array([10,13, 16,30, 33,23, 30,61, 62,45, 59,119, 116,90, 156,198, 373,326])
+    anchors = np.array([1.25,1.625, 2.0,3.75, 4.125,2.875, 1.875,3.8125, 3.875,2.8125, 3.6875,7.4375, 3.625,2.8125, 4.875,6.1875, 11.65625,10.1875])
+    ANCHORS = anchors.reshape(3, 3, 2)
+    
+    conv_shape       = tf.shape(conv_output)
+    batch_size       = conv_shape[0]
+    output_size      = conv_shape[1]
+
+    conv_output = tf.reshape(conv_output, (batch_size, output_size, output_size, 3, 5 + NUM_CLASS))
+
+    conv_raw_dxdy = conv_output[:, :, :, :, 0:2]
+    conv_raw_dwdh = conv_output[:, :, :, :, 2:4]
+    conv_raw_conf = conv_output[:, :, :, :, 4:5]
+    conv_raw_prob = conv_output[:, :, :, :, 5: ]
+
+    y = tf.tile(tf.range(output_size, dtype=tf.int32)[:, tf.newaxis], [1, output_size])
+    x = tf.tile(tf.range(output_size, dtype=tf.int32)[tf.newaxis, :], [output_size, 1])
+
+    xy_grid = tf.concat([x[:, :, tf.newaxis], y[:, :, tf.newaxis]], axis=-1)
+    xy_grid = tf.tile(xy_grid[tf.newaxis, :, :, tf.newaxis, :], [batch_size, 1, 1, 3, 1])
+    xy_grid = tf.cast(xy_grid, tf.float32)
+
+    pred_xy = (tf.sigmoid(conv_raw_dxdy) + xy_grid) * STRIDES[i]
+    pred_wh = (tf.exp(conv_raw_dwdh) * ANCHORS[i]) * STRIDES[i]
+    pred_xywh = tf.concat([pred_xy, pred_wh], axis=-1)
+
+    pred_conf = tf.sigmoid(conv_raw_conf)
+    pred_prob = tf.sigmoid(conv_raw_prob)
+
+    return tf.concat([pred_xywh, pred_conf, pred_prob], axis=-1)    
+
+INPUT_SIZE = 608
+
+def build_model(weight_file):
+
+    input_layer  = tf.keras.layers.Input([INPUT_SIZE, INPUT_SIZE, 3])
+    feature_maps = YOLOv3(input_layer)
+
+    feature_maps_array = np.array(feature_maps)
+
+    bbox_tensors = []
+    for i, fm in enumerate(feature_maps):
+        bbox_tensor = decode(fm, i)
+        bbox_tensors.append(bbox_tensor)
+    # Load Weights
+    model = tf.keras.Model(input_layer, bbox_tensors)
+    utils.load_weights(model, weight_file)
+    # model.summary()    
+
+    return model
+
+def do_predict(model, image_data):
+
+    pred_bbox = model.predict(image_data)
+    pred_bbox = [tf.reshape(x, (-1, tf.shape(x)[-1])) for x in pred_bbox]
+    pred_bbox = tf.concat(pred_bbox, axis=0)
+
+    pred_bbox_array = np.array(pred_bbox)
+    bboxes = utils.postprocess_boxes(pred_bbox, original_image_size, INPUT_SIZE, 0.05)
+    bboxes = utils.nms(bboxes, 0.45, method='nms')
+
+    return bboxes
+
+class YoloV3Wrapper():
+    def __init__(self, weight_file):
+        self.model = build_model(weight_file)
+        self.input_shape = (608, 608)
+        self.INPUT_SIZE = INPUT_SIZE
+
+    def predict(self, image_data):
+        return do_predict(self.model, image_data)
